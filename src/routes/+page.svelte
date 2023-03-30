1ee364e2f1a112fbaf143b1c26b09bb5c2eea803
TellMin <dhgirkcls@gmail.com>
2023-03-30 21:35:09 +0900

    Add openaiAPI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import type { ActionData } from './$types';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<h1>Let's get cracking bones!</h1>
+	export let form: ActionData;
+</script>
+
+<div class="card p-4 text-surface-700-200-token">
+	<form method="POST" action="?/chat">
+		<label class="label pb-4">
+			<h1>Input</h1>
+			<textarea name="message" class="input" placeholder="Input" />
+		</label>
+
+		<button type="submit" class="btn btn-sm variant-filled-tertiary">send</button>
+	</form>
+
+	<div class="card p-4 mt-4 variant-filled-surface">
+		{#if form}
+			{form.text}
+		{/if}
+	</div>
 </div>
